d83d2c4b20eff12f7f247a56e12fe6e7615a2915
shayan <koushashayan@gmail.com>
2018-05-09 22:35:58 -0400

    landmar_recognition.py is ready to be trained

--- script/landmark_recognition.py
+++ script/landmark_recognition.py
@@ -3,10 +3,21 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 import tensorflow as tf
 from datetime import datetime
-import os, csv, sys, glob, pickle
+import os, csv, sys, glob, pickle, time
 import cv2 as cv
 
-def recognition_model (features, labels, num_classes):
+def write_results(ids, landmarks, step, csv_path="../data/submission.csv"):
+    with open(csv_path, 'ab') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        
+        if step==0:
+            writer.writerow(['id', 'landmarks'])
+        
+        for im in range(len(ids)):
+            writer.writerow([str(ids[im]), str(landmarks[im])])
+    
+def recognition_model (features, num_classes):
     # conv layer 1
     conv1 = tf.layers.conv2d(inputs=features, filters=48, kernel_size=11, strides=4, activation=tf.nn.relu)
     pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=3, strides=2)
@@ -50,15 +61,19 @@ def accuracy_cal(prediction, y):
     
     return accuracy
     
-def load_patch (label_dict, images, batch_size, step):
+def load_patch (images, batch_size, label_dict=None, step=0):
     stack_images = None
     labels = []
-
+    names = []
+    
     for im in range(batch_size):
         image_path = images[im + batch_size * step]
         image_name = image_path.split('/')[-1].split('.')[0]
         
-        labels.append(int(label_dict[image_name]))
+        names.append(image_name)
+        
+        if not(label_dict is None):
+            labels.append(int(label_dict[image_name]))
         
         image = cv.imread(image_path)
         
@@ -67,12 +82,12 @@ def load_patch (label_dict, images, batch_size, step):
         else:
             stack_images = np.vstack((stack_images, image[np.newaxis, :, :]))
 
-    return stack_images, labels
+    return stack_images, labels, names
 
 def train_loop (label_dict, image_paths, mode):
     # TODO 
-    epoch_number = 10
-    batch_size = 5
+    epoch_number = 100
+    batch_size = 128
     num_classes = 15000
     
     num_batches = int(np.floor(len(image_paths)/batch_size))
@@ -83,7 +98,8 @@ def train_loop (label_dict, image_paths, mode):
     pr = tf.placeholder(tf.float32, [batch_size, num_classes])
     label = tf.placeholder(tf.int32, [batch_size])
     
-    model = recognition_model(f, l, num_classes)
+    #model = recognition_model(f, l, num_classes)
+    model = recognition_model(f, num_classes)
     
     loss = tf.losses.sparse_softmax_cross_entropy(labels=l, logits=model)
 
@@ -95,31 +111,45 @@ def train_loop (label_dict, image_paths, mode):
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         
-# =============================================================================
-#         saver = tf.train.Saver()
-#         try:
-#             saver.restore(sess, "./checkpoint/model.ckpt")
-#         except tf.errors.NotFoundError:
-#             pass
-# =============================================================================
+        saver = tf.train.Saver()
+        saver.restore(sess, "./checkpoints/model.ckpt")
+
+        if int(mode) == 0:
+            print("{} Start training...".format(datetime.now()))
+            start_time = time.time()
             
-        print("{} Start training...".format(datetime.now()))
+            for epoch in range(epoch_number):
+                sum_loss = 0
+                count = 0
+                
+                for step in range(num_batches):
+                    images, labels, names = load_patch(image_paths, batch_size, label_dict, step)
+                    logits, _, ret_loss = sess.run([model, train_op, loss], feed_dict={f: images, l: labels})
+                    acc = sess.run(accuracy, feed_dict={pr: logits, label: labels})
+                    
+                    sum_loss += ret_loss
+                    count += 1
+                    
+                    if step % 100 == 0:
+                        print("Epoch {}, Step {}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch, step, sum_loss/count, acc))
+                        count = sum_loss = 0
+                    
+                    
+                print("epoch = {}, execution time = {}".format(epoch, str(time.time() - start_time)))
+                print(len(tf.get_default_graph().get_operations()))
+                
+                save_path = saver.save(sess, "./checkpoints/model_epoch_" + str(epoch) + ".ckpt" )
         
-        for epoch in range(epoch_number):
-            sum_loss = 0
-            count = 0
+        elif int(mode) == 1:
+            print("{} Start predicting...".format(datetime.now()))
             
             for step in range(num_batches):
-                images, labels = load_patch(label_dict, image_paths, batch_size, step)
-                logits, _, ret_loss = sess.run([model, train_op, loss], feed_dict={f: images, l: labels})
-                acc = sess.run(accuracy, feed_dict={pr: logits, label: labels})
-                
-                sum_loss += ret_loss
-                count += 1
-                
-                #if step % 100 == 0:
-                print("Epoch {}, Step {}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch, step, sum_loss/count, acc))
-                count = sum_loss = 0
+                images, _, names = load_patch(image_paths, batch_size, step=step)
+                logits = sess.run(model, feed_dict={f: images})
+                prediction = tf.argmax(logits, 1)
+                #print(names)
+                #print(prediction.eval())
+                write_results(ids=names, landmarks=prediction.eval(), step=step)
                 
 def main (argv):
     if len(argv) != 3:
@@ -127,7 +157,7 @@ def main (argv):
         sys.exit(0)
         
     (image_path, mode) = argv[1:]
-        
+
     image_paths = glob.glob(image_path + "*.jpg")
 
     with open('../data/train.pkl', 'rb') as handle:
